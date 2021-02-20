1645f55096942bb07db11a7e3ab4c1dd7b71f2bc
JapanPanda <japanxpanda@yahoo.com>
2021-02-20 13:15:46 -0800

    Finalized merge

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import app from "./firebase.js";
+import app from "./Firebase.js";
 
 export const AuthContext = React.createContext();
 
@@ -12,32 +12,35 @@ export const AuthProvider = ({ children }) => {
     app.auth().onAuthStateChanged((user) => {
       var data;
       var docRef = app.firebase().collection.doc(user.uid);
-      docRef.get().then(doc) => {
-        if (doc.exists) {
-          data = doc.data();
-          console.log("Document data:", data);
-        } else {
-          console.log("No such document!");
-        }
-      }).catch((error) => {
-        console.log("Error getting document:", error);
-      });
+      docRef
+        .get()
+        .then((doc) => {
+          if (doc.exists) {
+            data = doc.data();
+            console.log("Document data:", data);
+          } else {
+            console.log("No such document!");
+          }
+        })
+        .catch((error) => {
+          console.log("Error getting document:", error);
+        });
 
-      setCurrentUser(user)
-      setCurrentData(data)
-      setPending(false)
+      setCurrentUser(user);
+      setCurrentData(data);
+      setPending(false);
     });
   }, []);
 
   if (pending) {
-    return <>Loading...</>
+    return <>Loading...</>;
   }
 
   return (
     <AuthContext.Provider
       value={{
         currentUser,
-        currentData
+        currentData,
       }}
     >
       {children}
