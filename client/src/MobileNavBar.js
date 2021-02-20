f96829d94f7c64f6343c450346698b812269ee4b
JapanPanda <japanxpanda@yahoo.com>
2021-02-20 01:39:15 -0800

    Fixed images

--- client/src/MobileNavBar.js
+++ client/src/MobileNavBar.js
@@ -1,8 +1,6 @@
 import { TabBar } from 'antd-mobile';
 import { SELECTION_INVERT } from 'antd/lib/table/hooks/useSelection';
 import React from 'react';
-import mapNavBarIcon from './public/mapNavBarIcon.png'; 
-
 
 class TabBarExample extends React.Component {
   constructor(props) {
@@ -16,36 +14,53 @@ class TabBarExample extends React.Component {
 
   renderContent(pageText) {
     return (
-      <div style={{ backgroundColor: 'white', height: '100%', textAlign: 'center' }}>
-        <div style={{ paddingTop: 60 }}>Clicked “{pageText}” tab， show “{pageText}” information</div>
+      <div
+        style={{
+          backgroundColor: 'white',
+          height: '100%',
+          textAlign: 'center',
+        }}>
+        <div style={{ paddingTop: 60 }}>
+          Clicked “{pageText}” tab， show “{pageText}” information
+        </div>
       </div>
     );
   }
 
   render() {
     return (
-      <div style={this.state.fullScreen ? { position: 'fixed', height: '100%', width: '100%', top: 0 } : { height: 400 }}>
+      <div
+        style={
+          this.state.fullScreen
+            ? { position: 'fixed', height: '100%', width: '100%', top: 0 }
+            : { height: 400 }
+        }>
         <TabBar
-          unselectedTintColor="#949494"
-          tintColor="#33A3F4"
-          barTintColor="white"
-          hidden={this.state.hidden}
-        >
+          unselectedTintColor='#949494'
+          tintColor='#33A3F4'
+          barTintColor='white'
+          hidden={this.state.hidden}>
           <TabBar.Item
-            title="Map"
-            key="Map"
-            icon={<div style={{
-              width: '22px',
-              height: '22px',
-              // https://opensource.org/licenses/MIT liscense for the image below 
-              background: 'url(https://cdn4.iconfinder.com/data/icons/evil-icons-user-interface/64/location-512.png) center center /  21px 21px no-repeat' }}
-            />
+            title='Map'
+            key='Map'
+            icon={
+              <div
+                style={{
+                  width: '22px',
+                  height: '22px',
+                  // https://opensource.org/licenses/MIT liscense for the image below
+                  background: `url(${process.env.PUBLIC_URL}/assets/mapNavBarIcon.png) center center /  21px 21px no-repeat`,
+                }}
+              />
             }
-            selectedIcon={<div style={{
-              width: '22px',
-              height: '22px',
-              background: {mapNavBarIcon} }}
-            />
+            selectedIcon={
+              <div
+                style={{
+                  width: '22px',
+                  height: '22px',
+                  background: `url(${process.env.PUBLIC_URL}/assets/mapNavBarIcon.png) center center /  21px 21px no-repeat`,
+                }}
+              />
             }
             selected={this.state.selectedTab === 'blueTab'}
             badge={1}
@@ -54,27 +69,32 @@ class TabBarExample extends React.Component {
                 selectedTab: 'blueTab',
               });
             }}
-            data-seed="logId"
-          >
+            data-seed='logId'>
             {this.renderContent('Life')}
           </TabBar.Item>
           <TabBar.Item
             icon={
-              <div style={{
-                width: '22px',
-                height: '22px',
-                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat' }}
+              <div
+                style={{
+                  width: '22px',
+                  height: '22px',
+                  background:
+                    'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat',
+                }}
               />
             }
             selectedIcon={
-              <div style={{
-                width: '22px',
-                height: '22px',
-                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat' }}
+              <div
+                style={{
+                  width: '22px',
+                  height: '22px',
+                  background:
+                    'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat',
+                }}
               />
             }
-            title="Koubei"
-            key="Koubei"
+            title='Koubei'
+            key='Koubei'
             badge={'new'}
             selected={this.state.selectedTab === 'redTab'}
             onPress={() => {
@@ -82,49 +102,58 @@ class TabBarExample extends React.Component {
                 selectedTab: 'redTab',
               });
             }}
-            data-seed="logId1"
-          >
+            data-seed='logId1'>
             {this.renderContent('Koubei')}
           </TabBar.Item>
           <TabBar.Item
             icon={
-              <div style={{
-                width: '22px',
-                height: '22px',
-                background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat' }}
+              <div
+                style={{
+                  width: '22px',
+                  height: '22px',
+                  background:
+                    'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat',
+                }}
               />
             }
             selectedIcon={
-              <div style={{
-                width: '22px',
-                height: '22px',
-                background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat' }}
+              <div
+                style={{
+                  width: '22px',
+                  height: '22px',
+                  background:
+                    'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat',
+                }}
               />
             }
-            title="Friend"
-            key="Friend"
+            title='Friend'
+            key='Friend'
             dot
             selected={this.state.selectedTab === 'greenTab'}
             onPress={() => {
               this.setState({
                 selectedTab: 'greenTab',
               });
-            }}
-          >
+            }}>
             {this.renderContent('Friend')}
           </TabBar.Item>
           <TabBar.Item
-            icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
-            selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
-            title="My"
-            key="my"
+            icon={{
+              uri:
+                'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg',
+            }}
+            selectedIcon={{
+              uri:
+                'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg',
+            }}
+            title='My'
+            key='my'
             selected={this.state.selectedTab === 'yellowTab'}
             onPress={() => {
               this.setState({
                 selectedTab: 'yellowTab',
               });
-            }}
-          >
+            }}>
             {this.renderContent('My')}
           </TabBar.Item>
         </TabBar>
