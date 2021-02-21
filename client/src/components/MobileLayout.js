353e9744e228fd6042857e4176ec1ec92f82b6eb
JapanPanda <japanxpanda@yahoo.com>
2021-02-20 16:22:32 -0800

    Fixed hamburger menu

--- client/src/components/MobileLayout.js
+++ client/src/components/MobileLayout.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import './MobileLayout.css';
-import { TabBar } from 'antd-mobile';
+import React from "react";
+import "./MobileLayout.css";
+import { TabBar } from "antd-mobile";
 
-import { Button } from 'antd';
+import { Button } from "antd";
 
-import MobileNavBar from './MobileNavBar';
-import HamBurgerMenu from './HamburgerMenu'
-import Map from './Map';
+import MobileNavBar from "./MobileNavBar";
+import HamBurgerMenu from "./HamburgerMenu";
+import Map from "./Map";
 
 const MobileLayout = () => {
   return (
-    <div> 
-      <HamBurgerMenu/>
-      <MobileNavBar/>
+    <div>
+      <MobileNavBar />
     </div>
   );
 };
