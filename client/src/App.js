d4bfd24abcb1a547593af3b2428cac45f03b4ad6
Derick Phan <dtph@ucdavis.edu>
2021-02-20 22:11:16 -0800

    initialized currentProfile

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect, useContext } from "react";
 import './App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import 'antd/dist/antd.css';
@@ -6,10 +7,32 @@ import { isMobile } from 'react-device-detect';
 import MobileLayout from './components/MobileLayout';
 import Login from "./components/MobileLogin";
 import Signup from "./components/MobileSignup";
-import { AuthProvider } from "./components/Auth";
+import { AuthContext, AuthProvider } from "./components/Auth";
 import PrivateRoute from "./components/PrivateRoute";
+import app from "./components/firebase"
 
 function App() {
+  const [currentUser] = useContext(AuthContext);
+  const [currentProfile, setCurrentProfile] = useState(null);
+
+  useEffect(() => {
+    if (!currentUser) return;
+    var docRef = app.firestore().collection('users').doc(currentUser.uid);
+    docRef
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          setCurrentProfile(doc.data);
+          console.log("Document data:", doc.data);
+        } else {
+          console.log("No such document!");
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document:", error);
+      });
+  }, [currentUser]);
+
   return (
     <div className='App'>
       <AuthProvider>
