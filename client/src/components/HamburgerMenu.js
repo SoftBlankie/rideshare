5357c42dbe05ceed57df54ca3c54d2b33009882b
Derick Phan <dtph@ucdavis.edu>
2021-02-21 06:22:30 -0800

    changed menu width

--- client/src/components/HamburgerMenu.js
+++ client/src/components/HamburgerMenu.js
@@ -51,7 +51,7 @@ class HamBurgerMenu extends React.Component {
         </div>
         <div className="drawer-div">
           <Drawer
-            width="40%"
+            width="60%"
             title={profileName}
             placement="left"
             closable={false}
