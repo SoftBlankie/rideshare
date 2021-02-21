ec9fb8ab77f3a44893aea234eea3302c0ace8602
JapanPanda <japanxpanda@yahoo.com>
2021-02-20 21:11:24 -0800

    Cleaned up login and signup form

--- client/src/components/MobileSignup.js
+++ client/src/components/MobileSignup.js
@@ -12,8 +12,8 @@ import {
   Checkbox,
   Button,
   AutoComplete,
-} from 'antd';
-import { QuestionCircleOutlined } from '@ant-design/icons';
+} from "antd";
+import { QuestionCircleOutlined } from "@ant-design/icons";
 
 const { Option } = Select;
 
@@ -46,171 +46,171 @@ const Signup = ({ history }) => {
   const prefixSelector = (
     <Form.Item name="prefix" noStyle>
       <Select style={{ width: 70 }}>
+        <Option value="1">+1</Option>
         <Option value="86">+86</Option>
         <Option value="87">+87</Option>
       </Select>
     </Form.Item>
   );
 
-  const handleSignup = useCallback(async event => {
-    event.preventDefault();
-    const {
-      email,
-      password,
-      name,
-      address,
-      phone
-    } = event.target.elements;
-    try {
-      await app
-        .auth()
-        .createUserWithEmailAndPassword(email.value, password.value).then(cred => {
-          return app.firestore().collection('users').doc(cred.user.uid).set({
-            name: name.value,
-            address: address.value,
-            phone: phone.value
+  const handleSignup = useCallback(
+    async (event) => {
+      event.preventDefault();
+      const { email, password, name, address, phone } = event.target.elements;
+      try {
+        await app
+          .auth()
+          .createUserWithEmailAndPassword(email.value, password.value)
+          .then((cred) => {
+            return app.firestore().collection("users").doc(cred.user.uid).set({
+              name: name.value,
+              address: address.value,
+              phone: phone.value,
+            });
           });
-        });;
-      history.push("/");
-    } catch (error) {
-      alert(error);
-    }
-  }, [history]);
+        history.push("/");
+      } catch (error) {
+        alert(error);
+      }
+    },
+    [history]
+  );
 
   return (
-    <Form
-      {...formItemLayout}
-      form={form}
-      name="register"
-      onFinish={handleSignup}
-      initialValues={{
-        prefix: '86',
-      }}
-      scrollToFirstError
-    >
-      <Form.Item
-        name="email"
-        label="E-mail"
-        rules={[
-          {
-            type: 'email',
-            message: 'The input is not valid E-mail!',
-          },
-          {
-            required: true,
-            message: 'Please input your E-mail!',
-          },
-        ]}
+    <div className="form">
+      <div className="header">
+        <h1>Signup</h1>
+      </div>
+      <Form
+        {...formItemLayout}
+        form={form}
+        name="register"
+        onFinish={handleSignup}
+        initialValues={{
+          prefix: "86",
+        }}
+        scrollToFirstError
       >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        name="password"
-        label="Password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item
-        name="confirm"
-        label="Confirm Password"
-        dependencies={['password']}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: 'Please confirm your password!',
-          },
-          ({ getFieldValue }) => ({
-            validator(_, value) {
-              if (!value || getFieldValue('password') === value) {
-                return Promise.resolve();
-              }
-              return Promise.reject('The two passwords that you entered do not match!');
+        <Form.Item
+          name="email"
+          rules={[
+            {
+              type: "email",
+              message: "The input is not valid E-mail!",
             },
-          }),
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
+            {
+              required: true,
+              message: "Please input your E-mail!",
+            },
+          ]}
+        >
+          <Input placeholder="Email" />
+        </Form.Item>
 
-      <Form.Item
-        name="name"
-        label={
-          <span>
-            Name&nbsp;
-            <Tooltip title="What do you want others to call you?">
-              <QuestionCircleOutlined />
-            </Tooltip>
-          </span>
-        }
-        rules={[{ required: true, message: 'Please input your name!', whitespace: true }]}
-      >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+          hasFeedback
+        >
+          <Input.Password placeholder="Password" />
+        </Form.Item>
 
-      <Form.Item
-        name="address"
-        label="Address"
-        rules={[{ required: true, message: 'Please input your address!' }]}
-      >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          name="confirm"
+          dependencies={["password"]}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: "Please confirm your password!",
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  "The two passwords that you entered do not match!"
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password placeholder="Confirm Password" />
+        </Form.Item>
 
-      <Form.Item
-        name="phone"
-        label="Phone Number"
-        rules={[{ required: true, message: 'Please input your phone number!' }]}
-      >
-        <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
-      </Form.Item>
+        <Form.Item
+          name="name"
+          rules={[
+            {
+              required: true,
+              message: "Please input your name!",
+              whitespace: true,
+            },
+          ]}
+        >
+          <Input placeholder="Name" />
+        </Form.Item>
 
-      <Form.Item label="Captcha" extra="We must make sure that you are a human.">
-        <Row gutter={8}>
-          <Col span={12}>
-            <Form.Item
-              name="captcha"
-              noStyle
-              rules={[{ required: true, message: 'Please input the captcha you got!' }]}
+        <Form.Item
+          name="address"
+          rules={[{ required: true, message: "Please input your address!" }]}
+        >
+          <Input placeholder="Address" />
+        </Form.Item>
+
+        <Form.Item
+          name="phone"
+          rules={[
+            { required: true, message: "Please input your phone number!" },
+          ]}
+        >
+          <Input
+            placeholder="Phone"
+            addonBefore={prefixSelector}
+            style={{ width: "100%" }}
+          />
+        </Form.Item>
+
+        <Form.Item
+          name="agreement"
+          valuePropName="checked"
+          rules={[
+            {
+              validator: (_, value) =>
+                value
+                  ? Promise.resolve()
+                  : Promise.reject("Should accept agreement"),
+            },
+          ]}
+          {...tailFormItemLayout}
+        >
+          <Checkbox>
+            I have read the <a href="">agreement</a>
+          </Checkbox>
+        </Form.Item>
+        <Form.Item {...tailFormItemLayout}>
+          <div className="login-button-group">
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
             >
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Button>Get captcha</Button>
-          </Col>
-        </Row>
-      </Form.Item>
+              Sign up
+            </Button>
 
-      <Form.Item
-        name="agreement"
-        valuePropName="checked"
-        rules={[
-          {
-            validator: (_, value) =>
-              value ? Promise.resolve() : Promise.reject('Should accept agreement'),
-          },
-        ]}
-        {...tailFormItemLayout}
-      >
-        <Checkbox>
-          I have read the <a href="">agreement</a>
-        </Checkbox>
-      </Form.Item>
-      <Form.Item {...tailFormItemLayout}>
-        <Button type="primary" htmlType="submit">
-          Register
-        </Button>
-      </Form.Item>
-    </Form>
+            <Button style={{ marginLeft: "10px" }} type="primary" href="/login">
+              Log in
+            </Button>
+          </div>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
