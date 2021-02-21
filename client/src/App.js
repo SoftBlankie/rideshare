64eb54950ff07ad5682005ea9f9f218745dcd93c
Derick Phan <dtph@ucdavis.edu>
2021-02-21 01:22:54 -0800

    merged

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect, useContext } from "react";
 import './App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import 'antd/dist/antd.css';
@@ -7,37 +6,17 @@ import { isMobile } from 'react-device-detect';
 import MobileLayout from './components/MobileLayout';
 import Login from "./components/MobileLogin";
 import Signup from "./components/MobileSignup";
-import { AuthContext, AuthProvider } from "./components/Auth";
+import Profile from "./components/MobileProfile";
+import { AuthProvider } from "./components/Auth";
 import PrivateRoute from "./components/PrivateRoute";
-import app from "./components/firebase"
 
 function App() {
-  const [currentUser] = useContext(AuthContext);
-  const [currentProfile, setCurrentProfile] = useState(null);
-
-  useEffect(() => {
-    if (!currentUser) return;
-    var docRef = app.firestore().collection('users').doc(currentUser.uid);
-    docRef
-      .get()
-      .then((doc) => {
-        if (doc.exists) {
-          setCurrentProfile(doc.data);
-          console.log("Document data:", doc.data);
-        } else {
-          console.log("No such document!");
-        }
-      })
-      .catch((error) => {
-        console.log("Error getting document:", error);
-      });
-  }, [currentUser]);
-
   return (
     <div className='App'>
       <AuthProvider>
         <Router>
-          <PrivateRoute exact path="/" component={isMobile && MobileLayout} />
+          <PrivateRoute exact path="/" component={MobileLayout} />
+          <PrivateRoute exact path="/profile" component={Profile} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
         </Router>
